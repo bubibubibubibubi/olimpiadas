ff2166dda6d655e76335627e023fe104673fe2ca
bubibubibubibubi <mauricio424242@gmail.com>
2024-09-08 21:31:42 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+
+{
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira, campeã olímpica e um dos maiores nomes do judô mundial. Com sua força, técnica e determinação, conquistou diversas medalhas em competições internacionais, incluindo o ouro olímpico em Paris 2024.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza", 
+    tags: "ouro luta judoka"
+},
+{
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal é uma skatista brasileira, conhecida como a Fada, que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica brasileira.</p>",
+    link: "https://ko.wikipedia.org/wiki/%ED%95%98%EC%9D%B4%EC%82%AC_%EB%A0%88%EC%95%84%EC%9A%B0" ,
+    tags: "sk8 prata"
+},
+{
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é um fenômeno da ginástica artística brasileira e mundial. Com sua força, determinação e talento inigualáveis, ela conquistou o coração de milhões de fãs.</p>",
+    link: "https://ko.wikipedia.org/wiki/%ED%95%98%EC%9D%B4%EC%82%AC_%EB%A0%88%EC%95%84%EC%9A%B0" ,
+    tags: "ouro pulos ginastica"
+}
+];
